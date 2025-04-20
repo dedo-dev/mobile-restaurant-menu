a658dbd88439d84ec36c3f17de4b8e282d914b6b
Daniele Zeppieri <120616847+dedo-dev@users.noreply.github.com>
2025-04-20 18:17:39 +0100

    basic structure to render item and classes added

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 import {menuArray} from '/data.js'
 
-console.log(menuArray)
+let renderMenu = ''
+menuArray.forEach(item => {
+    renderMenu += `
+        <article class="menu">
+            <p class="menu__emoji">${item.emoji}</p>
+            <h3 class="menu__name">${item.name}</h3>
+            <h4 class="menu__ingredient">${item.ingredients}</h4>
+            <p class="menu__price">${item.price}</p>
+        </article>
+    `
+})
+
+document.getElementById('app').innerHTML = renderMenu
