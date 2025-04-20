75b0c06eed77170da197032c483054841a38ea7c
Daniele Zeppieri <120616847+dedo-dev@users.noreply.github.com>
2025-04-20 19:12:18 +0100

    button added to the items box menu

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ menuArray.forEach(item => {
     renderMenu += `
         <article class="menu">
             <p class="menu__emoji">${item.emoji}</p>
-            <h3 class="menu__name">${item.name}</h3>
-            <h4 class="menu__ingredient">${item.ingredients}</h4>
-            <p class="menu__price">${item.price}</p>
+            <div class="menu__content">
+                <h3 class="menu__name">${item.name}</h3>
+                <h4 class="menu__ingredient">${item.ingredients.join(', ')}</h4>
+                <p class="menu__price">$${item.price}</p>
+            </div>
+            <button>+</button>
         </article>
     `
 })
