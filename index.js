a5d57589fde43f07f07b6f39a8f6c4d2b2c3e612
Daniele Zeppieri <120616847+dedo-dev@users.noreply.github.com>
2025-04-20 20:31:10 +0100

    order section hard coded to apply style

--- index.js
+++ index.js
@@ -10,9 +10,20 @@ menuArray.forEach(item => {
                 <h4 class="menu__ingredient">${item.ingredients.join(', ')}</h4>
                 <p class="menu__price">$${item.price}</p>
             </div>
-            <button>+</button>
+            <button id="menu-btn" class="menu__btn">+</button>
         </article>
     `
 })
 
-document.getElementById('app').innerHTML = renderMenu
+document.getElementById('order-menu').innerHTML = renderMenu
+
+document.getElementById('order-item').innerHTML = `
+    <h2 style="color: black">Your Order</h2>
+    <article>
+        <p>Pizza</p>
+        <p>Beer</p>
+    </article>
+
+    <p>Total price:</p>
+    <button>Complete order</button>
+`
